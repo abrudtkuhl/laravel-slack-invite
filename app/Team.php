ed82c6541e90c22d68bbb2bede5aba82e95a49cb
Andy Brudtkuhl <abrudtkuhl@pork.org>
2016-06-07 23:33:18 -0500

    User::Team model relationship

--- app/Team.php
+++ app/Team.php
@@ -6,5 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Team extends Model
 {
-    //
+    protected $fillable = [
+        'name', 'token',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
