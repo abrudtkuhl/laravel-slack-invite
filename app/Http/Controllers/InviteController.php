05cf3c73e54607822123df738c3a46b09f0648ac
Andy Brudtkuhl <abrudtkuhl@pork.org>
2016-06-10 15:11:25 -0500

    formatting

--- app/Http/Controllers/InviteController.php
+++ app/Http/Controllers/InviteController.php
@@ -3,12 +3,12 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
 use App\Http\Requests;
 
 class InviteController extends Controller
 {
-    public function invite() {
+    public function invite()
+    {
         $email = Request::get('email');
         $this->dispatch(new SendInvite($email));
         return view('success');
